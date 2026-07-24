51f1f5eb76b79b1d293db872ccc645ef8d2eec6f
Pamela Shaw <Pamela.a.shaw@kp.org>
2026-07-24 13:51:05 -0700

    Add files via upload

---
+++ AllPackages.R
@@ -0,0 +1,10 @@
+library(MASS) ### stuff
+library(mvtnorm) ## for data generation
+library(survey) ### need to use sandwich2stage
+library(devtools) ### need to install sandwich2stage
+library(boot) # for bootstrap SE
+library(simex) ## Simex 
+###library(simexaft) ## Hands on session: removed from cran see Practicum 1 instructions for downloadig
+library(devtools) ## Needed to install simex and sandwich2stage for Practicum 1
+library(openxlsx) ## to read excel files
+library(episensr) ## For misclassification
